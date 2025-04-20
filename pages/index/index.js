fd181e68d69dd95e0e4f9ca6aec81b95be9a0626
jiale720 <j195874@qq.com>
2025-04-20 20:17:46 +0800

    11

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,9 @@
 Page({
   data: {
-    showMask: false,//遮罩层开关
+    showMask: false, //遮罩层开关
     currentTab: 0,
     titleHeight: 0,
-    tabHeight:0,
+    tabHeight: 0,
     activeType: null,
     cartItems: [],
     totalPrice: 0,
@@ -11,14 +11,14 @@ Page({
     showCartPopup: false,
     showProductDetails: false,
     selectedProduct: null,
-    dishList: [
-      {
+    goodsList: [{
         code: 1,
         name: '菜疙瘩',
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 12,
-        type:"PIG"
+        type: "PIG",
+        count: 0
       },
       {
         code: 2,
@@ -27,15 +27,17 @@ Page({
         specification: "10支/盒",
         desc: '近期100+人已下单',
         price: 16,
-        type:"PIG"
+        type: "PIG",
+        count: 0
       },
       {
         code: 3,
         name: '煎饼',
         img: '../images/pig1.jpg',
-        specification: "10支/盒",  
+        specification: "10支/盒",
         price: 7.5,
-        type:"OTHER"
+        type: "OTHER",
+        count: 0
       },
       {
         code: 4,
@@ -43,7 +45,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 16,
-        type:"FEED"
+        type: "FEED",
+        count: 0
       },
       {
         code: 5,
@@ -51,7 +54,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"FEED"
+        type: "FEED",
+        count: 0
       },
       {
         code: 6,
@@ -59,7 +63,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"PIG"
+        type: "PIG",
+        count: 0
       },
       {
         code: 7,
@@ -67,7 +72,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"SHEET"
+        type: "SHEET",
+        count: 0
       },
       {
         code: 8,
@@ -75,7 +81,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"SHEET"
+        type: "SHEET",
+        count: 0
       },
       {
         code: 9,
@@ -83,7 +90,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"CATTLE"
+        type: "CATTLE",
+        count: 0
       },
       {
         code: 10,
@@ -91,7 +99,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"CATTLE"
+        type: "CATTLE",
+        count: 0
       },
       {
         code: 11,
@@ -99,7 +108,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"CATTLE"
+        type: "CATTLE",
+        count: 0
       },
       {
         code: 12,
@@ -107,7 +117,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"OTHER"
+        type: "OTHER",
+        count: 0
       },
       {
         code: 13,
@@ -115,7 +126,8 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"OTHER"
+        type: "OTHER",
+        count: 0
       },
       {
         code: 14,
@@ -123,29 +135,29 @@ Page({
         img: '../images/pig1.jpg',
         specification: "10支/盒",
         price: 14.5,
-        type:"OTHER"
+        type: "OTHER",
+        count: 0
       }
     ],
-    typeList:[
-      {
-        name:"猪药",
-        value:"PIG"
+    typeList: [{
+        name: "猪药",
+        value: "PIG"
       },
       {
-        name:"牛药",
-        value:"CATTLE"
+        name: "牛药",
+        value: "CATTLE"
       },
       {
-        name:"羊药",
-        value:"SHEET"
+        name: "羊药",
+        value: "SHEET"
       },
       {
-        name:"饲料",
-        value:"FEED"
+        name: "饲料",
+        value: "FEED"
       },
       {
-        name:"其他",
-        value:"OTHER"
+        name: "其他",
+        value: "OTHER"
       }
     ],
     typePositions: {},
@@ -164,7 +176,7 @@ Page({
       currentTab: index
     });
   },
-  goToDetailPage: function(e) {
+  goToDetailPage: function (e) {
     wx.navigateTo({
       url: `/pages/detail/detail`
     });
@@ -175,64 +187,64 @@ Page({
   onReady() {
     this.getTitleHeight();
   },
-  getTitleHeight(){
+  getTitleHeight() {
     const query = wx.createSelectorQuery();
     let height = 0;
     query.selectAll('.sticky-element').boundingClientRect((rects) => {
-        if (rects) {
-          rects.forEach((rect, index) => {
-            height = rect.height;
-          });
-        }
-          this.setData({
-            tabHeight: height
-          },() => {
-            // 当 titleHeight 更新后，调用依赖 titleHeight 的方法
-            this.getTypePositions();
+      if (rects) {
+        rects.forEach((rect, index) => {
+          height = rect.height;
         });
+      }
+      this.setData({
+        tabHeight: height
+      }, () => {
+        // 当 titleHeight 更新后，调用依赖 titleHeight 的方法
+        this.getTypePositions();
       });
-      query.selectAll('.store-info').boundingClientRect((rects) => {
-        if (rects) {
-          rects.forEach((rect, index) => {
-            height = rect.height;
-          });
-        }
-          this.setData({
-            titleHeight: height
-          },() => {
-            // 当 titleHeight 更新后，调用依赖 titleHeight 的方法
-            this.getTypePositions();
+    });
+    query.selectAll('.store-info').boundingClientRect((rects) => {
+      if (rects) {
+        rects.forEach((rect, index) => {
+          height = rect.height;
         });
+      }
+      this.setData({
+        titleHeight: height
+      }, () => {
+        // 当 titleHeight 更新后，调用依赖 titleHeight 的方法
+        this.getTypePositions();
       });
+    });
     query.exec();
   },
   getTypePositions() {
     const query = wx.createSelectorQuery();
-    const typePositions = {}; 
+    const typePositions = {};
     const heights = [];
     this.data.typeList.forEach((type) => {
-        query.select(`.type-${type.value}`).boundingClientRect((rect) => {
-            if (rect) {
-                typePositions[type.value] = rect.top;
-            }
+      query.select(`.type-${type.value}`).boundingClientRect((rect) => {
+        if (rect) {
+          typePositions[type.value] = rect.top;
+        }
+      });
+      if (this.data.typeList[this.data.typeList.length - 1].value === type.value) {
+        query.select(`.${type.value}`).boundingClientRect((rect) => {
+          if (rect) {
+            heights.push(rect.height);
+          }
         });
-        if(this.data.typeList[this.data.typeList.length-1].value === type.value){
-          query.select(`.${type.value}`).boundingClientRect((rect) => {
-            if (rect) {
-              heights.push(rect.height);
-            }
-          }); 
-        }   
+      }
     });
     const tabHeight = this.data.tabHeight;
     query.selectViewport().boundingClientRect((viewportRect) => {
       if (viewportRect) {
         const totalHeight = heights.reduce((sum, height) => sum + height, 0);
         //这里控制右侧的类型元素可以在下滑时保持在顶部，-10 是为了防止类型名称被搜索框覆盖住
-        const blankSpaceHeight = viewportRect.height-totalHeight-tabHeight-10;
+        const blankSpaceHeight = viewportRect.height - totalHeight - tabHeight - 10;
         this.setData({
           typePositions,
-          blankSpaceHeight:blankSpaceHeight
+          blankSpaceHeight: blankSpaceHeight
         });
       }
     }).exec();
@@ -240,16 +252,19 @@ Page({
 
   onRightViewScroll(e) {
     const scrollTop = e.detail.scrollTop;
-    const { typeList, typePositions } = this.data;
-    const height = this.data.titleHeight+this.data.tabHeight;
+    const {
+      typeList,
+      typePositions
+    } = this.data;
+    const height = this.data.titleHeight + this.data.tabHeight;
     let activeType = null;
     typeList.forEach((type) => {
-        if (typePositions[type.value] && typePositions[type.value] <= scrollTop+height+40) {
-            activeType = type.value;
-        }
+      if (typePositions[type.value] && typePositions[type.value] <= scrollTop + height + 40) {
+        activeType = type.value;
+      }
     });
     this.setData({
-        activeType
+      activeType
     });
   },
   onParentViewTap() {
@@ -259,9 +274,8 @@ Page({
     const totalQuantity = e.detail;
     const product = e.currentTarget.dataset.product; // 获取当前商品信息
     this.addToCart(product);
-
     this.setData({
-      totalQuantity: totalQuantity
+      totalQuantity: totalQuantity,
     });
     console.log('当前数量是:', totalQuantity);
   },
@@ -270,7 +284,7 @@ Page({
     const titleHeight = this.data.titleHeight;
     const tabHeight = this.data.tabHeight;
     const type = e.currentTarget.dataset.type;
-    const position = this.data.typePositions[type] - titleHeight-tabHeight;
+    const position = this.data.typePositions[type] - titleHeight - tabHeight;
     if (position) {
       this.setData({
         scrollTop: position // 设置 scrollTop 来滚动到目标位置
@@ -306,10 +320,10 @@ Page({
   // 计算总价和总数量
   calculateTotal() {
     let totalPrice = 0;
-    let totalQuantity = 1;
+    let totalQuantity = 0;
     this.data.cartItems.forEach(item => {
-      totalPrice += item.price * item.quantity;
-      totalQuantity += item.quantity;
+      totalPrice += item.price * item.count;
+      totalQuantity += item.count;
     });
     this.setData({
       totalPrice,
@@ -317,95 +331,103 @@ Page({
     });
   },
   // 修改商品数量
-  changeQuantity(e) {
-    const productId = e.currentTarget.dataset.productId;
-    const delta = parseInt(e.currentTarget.dataset.delta);
+  changeCountInCart(e) {
+    debugger
+    const code = e.currentTarget.dataset.code;
+    const count = parseInt(e.detail);
     let cartItems = this.data.cartItems;
-    const itemIndex = cartItems.findIndex(item => item.productId === productId);
+    let goodsList = this.data.goodsList;
+    const itemIndex = cartItems.findIndex(item => item.code === code);
+    const goodsIndex = goodsList.findIndex(item => item.code === code);
     if (itemIndex > -1) {
-      cartItems[itemIndex].quantity += delta;
-      if (cartItems[itemIndex].quantity <= 0) {
+      cartItems[itemIndex].count = count;
+      if (cartItems[itemIndex].count <= 0) {
         cartItems.splice(itemIndex, 1);
       }
-      this.setData({
-        cartItems
-      });
-      this.calculateTotal();
-      if (cartItems.length === 0) {
-        this.setData({
-          showCartPopup: false
-        });
-      }
     }
-  },
-  // 清空购物车
-  clearCart() {
+    if (goodsIndex > -1) {
+      goodsList[goodsIndex].count = count;
+    }
     this.setData({
-      cartItems: [],
-      totalPrice: 0,
-      totalQuantity: 0,
-      showCartPopup: false,
-      showMask:false
+      cartItems,
+      goodsList
     });
-  },
-  // 隐藏购物车弹框
-  hideCartPopup() {
-    this.setData({
-      showCartPopup: false
-    });
-  },
-  // 显示购物车详情
-  showCartDetails() {
-    if (this.data.totalQuantity > 0) {
-      let isShow = this.data.showCartPopup;
-      let showMask = this.data.showMask;
-
-      if(isShow === true){
-        isShow = false;
-        showMask = false;
-      }else{
-        isShow = true;
-        showMask = true;
-      }
+    this.calculateTotal();
+    if (cartItems.length === 0) {
       this.setData({
-        showCartPopup: isShow,
-        showMask: showMask
+        showCartPopup: false
       });
     }
-    console.log(this.data.showCartPopup);
-  },
-  // 显示商品详情界面
-  showProductDetails(product) {
-    this.setData({
-      showProductDetails: true,
-      selectedProduct: product
-    });
-  },
-  // 隐藏商品详情界面
-  hideProductDetails() {
-    this.setData({
-      showProductDetails: false
-    });
-  },
-  // 跳转到订单页面
-  showOrderPopup() {
-    const cartItems = JSON.stringify(this.data.cartItems);
-    const totalPrice = this.data.totalPrice;
-    wx.navigateTo({
-      url: `/pages/order/order?cartItems=${cartItems}&totalPrice=${totalPrice}`
-    });
-  },
+  
+},
+// 清空购物车
+clearCart() {
+  this.setData({
+    cartItems: [],
+    totalPrice: 0,
+    totalQuantity: 0,
+    showCartPopup: false,
+    showMask: false
+  });
+},
+// 隐藏购物车弹框
+hideCartPopup() {
+  this.setData({
+    showCartPopup: false
+  });
+},
+// 显示购物车详情
+showCartDetails() {
+  if (this.data.totalQuantity > 0) {
+    let isShow = this.data.showCartPopup;
+    let showMask = this.data.showMask;
 
-  // 关闭弹出框
-  closeMask() {
+    if (isShow === true) {
+      isShow = false;
+      showMask = false;
+    } else {
+      isShow = true;
+      showMask = true;
+    }
     this.setData({
-      showMask: false,
-      showCartPopup: false
+      showCartPopup: isShow,
+      showMask: showMask
     });
-  },
+  }
+  console.log(this.data.showCartPopup);
+},
+// 显示商品详情界面
+showProductDetails(product) {
+  this.setData({
+    showProductDetails: true,
+    selectedProduct: product
+  });
+},
+// 隐藏商品详情界面
+hideProductDetails() {
+  this.setData({
+    showProductDetails: false
+  });
+},
+// 跳转到订单页面
+showOrderPopup() {
+  const cartItems = JSON.stringify(this.data.cartItems);
+  const totalPrice = this.data.totalPrice;
+  wx.navigateTo({
+    url: `/pages/order/order?cartItems=${cartItems}&totalPrice=${totalPrice}`
+  });
+},
 
-  preventTouchMove(e) {
-    // 不做任何操作，仅阻止事件冒泡
-  },
- 
+// 关闭弹出框
+closeMask() {
+  this.setData({
+    showMask: false,
+    showCartPopup: false
+  });
+},
+
+preventTouchMove(e) {
+  // 不做任何操作，仅阻止事件冒泡
+},
+
 })
